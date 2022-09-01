8c58c107ce73363e44673ff7192691fb3c6ac508
jjsabadopdaxph <82558808+jjsabadopdaxph@users.noreply.github.com>
2022-09-01 08:14:39 +0800

    Initial
    Push

---
+++ build/precache-manifest.a0a9b663cd0462955f8e9d955a5082f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1a80ff9f07f99538644",
+    "url": "/JuanPokeDex/static/css/main.3965dd95.chunk.css"
+  },
+  {
+    "revision": "a1a80ff9f07f99538644",
+    "url": "/JuanPokeDex/static/js/main.a1a80ff9.chunk.js"
+  },
+  {
+    "revision": "cc7899086d3329582051",
+    "url": "/JuanPokeDex/static/css/1.edacc240.chunk.css"
+  },
+  {
+    "revision": "cc7899086d3329582051",
+    "url": "/JuanPokeDex/static/js/1.cc789908.chunk.js"
+  },
+  {
+    "revision": "bf8c3a743c6b72ed2dd3",
+    "url": "/JuanPokeDex/static/js/runtime~main.bf8c3a74.js"
+  },
+  {
+    "revision": "ed1932c8117e9da365ab123ac17572d9",
+    "url": "/JuanPokeDex/index.html"
+  }
+];
